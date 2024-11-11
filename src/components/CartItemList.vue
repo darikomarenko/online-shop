e4937608cfb4ffc1225c1a42c48df157e91a6063
Daria Komarenko <dkomarenko@spherical.pm>
2024-11-11 14:07:38 +0300

    Add Button to Cart

---
+++ src/components/CartItemList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import CartItem from './CartItem.vue'
+</script>
+
+<template>
+  <div class="flex flex-col flex-1 gap-4 justify-between" v-auto-animate>
+    <CartItem
+      v-for="item in cart"
+      :key="item.id"
+      :title="item.title"
+      :price="item.price"
+      :image-url="item.imageUrl"
+      @on-click-remove="() => removeFromCart(item)"
+    />
+  </div>
+</template>
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -3,10 +3,32 @@
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
 
     <DrawerHead/>
+    <CartItemList/>
 
+    <div class="flex flex-col gap-4 my-6">
+      <div class="flex gap-2">
+        <span>Total:</span>
+        <div class="flex-1 border-bottom border-dashed"></div>
+        <b>12900$</b>
+      </div>
+
+      <div>
+      <div class="flex gap-2">
+        <span>Tax 5%:</span>
+        <div class="flex-1 border-bottom border-dashed"></div>
+        <b>900$</b>
+      </div>
+    </div>
+
+    </div>
+
+    <button class="bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 transition hover:bg-lime-700">
+      Order
+    </button>
   </div>
 </template>
 
-<script>
+<script setup>
 import DrawerHead from './DrawerHead.vue'
+import CartItemList from './CartItemList.vue'
 </script>
